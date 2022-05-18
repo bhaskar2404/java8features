8caf8bb96ce67b4f82ac3d815f32bb4a8a56e351
Bhaskar.vanam <bhaskar.vanam@gmail.com>
2022-05-18 18:26:11 +0530

    Stream is commiting

--- src/StreamApiExamples.java
+++ src/StreamApiExamples.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class StreamApiExamples {
@@ -9,7 +10,7 @@ public class StreamApiExamples {
 		
 		//Converting the String to uppercase and printing the values
 		
-		 List<String> stringList = Arrays.asList("John", "Martin", "Mary", "Steve");
+		 List<String> stringList = Arrays.asList("Vanam", "Bhaskar", "Geetha", "Josritha");
 		 
 	        stringList.stream()
 	                   .map((s) -> s.toUpperCase())
@@ -22,6 +23,27 @@ public class StreamApiExamples {
 	        Stream<Integer> strGen=Stream.generate(()->1).limit(10);
 	        
 	        strGen.forEach(System.out::println);
+	        
+	        Stream<String> StrString=Stream.of("Bhaskar","Vanam","Thanusri","Josritha","Geetha");
+	        Stream<String> nameStartWithSri  =StrString.map(String::toUpperCase).peek(s->System.out.println(s)).filter(s->s.contains("SRI"));
+	       System.out.println(nameStartWithSri.count());
+	       
+	       
+	       Stream<String> StrStringOrd=Stream.of("Bhaskar","Vanam","Thanusri","Josritha","Geetha");
+	       
+	       Stream<String> nameStartWithOrd=StrStringOrd.map((s)->{
+	    	   System.out.println("Map :"+s);
+	    	   return s.toLowerCase();
+	       }).filter((s)->{
+	    	   System.out.println("Filter:"+s);
+	    	   return s.contains("sri");
+	       });
+	       
+	       
+	       Optional<String> findAny=nameStartWithOrd.findAny();
+	       System.out.println(findAny);
+	       
+	       Stream.of("10","20","30","40").mapToInt(Integer::parseInt).average().ifPresent(System.out::println);
 	}
 
 }
